bc8124de248c973e3405fa53a063a4f7ce39db55
Brian Nelson <nelsonsbrian@gmail.com>
2018-08-10 11:35:09 -0700

    add BS modal for reccomendation template

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+  var tally1 = 0;
+  var tally2 = 0;
+  var tally3 = 0;
+
+  $("button#complete-reccommendation").click(function() {
+    if (tally1 === 0 && tally2 === 0 && tally3 === 0) {
+      $(".survey-complete").hide();
+
+    } else {
+      $(".no-survey").hide();
+
+    }
+
+  });
+
+
   $("button#survey-submit").click(function() {
 
     var answer1 = ($("input:radio[name=quest1]:checked").val());
@@ -6,14 +23,20 @@ $(document).ready(function(){
     var answer3 = ($("input:radio[name=quest3]:checked").val());
     var answer4 = ($("input:radio[name=quest4]:checked").val());
     var answer5 = ($("input:radio[name=quest5]:checked").val());
-    var tally1 = 0;
-    var tally2 = 0;
-    var tally3 = 0;
+    var answer6 = ($("input:radio[name=quest6]:checked").val());
 
     // alert(answer1 + " " + answer2 + " " + answer3 + " " + answer4 + " " + answer5);
-    for (i = 1; i <= 5; i++) {
-
-      if ((eval("answer" + i) < 40) && (eval("answer" + i) >= 30)) {
+    for (i = 1; i <= 6; i++) {
+      if ((eval("answer" + i) < 70) && (eval("answer" + i) >= 60)) {
+        tally1++;
+        tally2++;
+      } else if ((eval("answer" + i) < 60) && (eval("answer" + i) >= 50)) {
+        tally1++;
+        tally3++;
+      } else if ((eval("answer" + i) < 50) && (eval("answer" + i) >= 40)) {
+        tally2++;
+        tally3++;
+      } else if ((eval("answer" + i) < 40) && (eval("answer" + i) >= 30)) {
         tally3++;
       } else if ((eval("answer" + i) < 30) && (eval("answer" + i) >= 20)) {
         tally2++;
@@ -23,11 +46,24 @@ $(document).ready(function(){
     };
 
     alert("1: " + tally1 + " 2: " + tally2 + " 3: " + tally3)
-
-
-    });
-
-
-
-
+    if (tally1 > tally2 && tally1 > tally3) {
+      alert("sequence#1");
+    } else if (tally2 > tally1 && tally2 > tally3) {
+      alert("sequence#2");
+    } else if (tally3 > tally1 && tally3 > tally2) {
+      alert("sequence#3");
+    } else if (tally1 === tally2 && tally1 === tally3 && tally2 === tally3) {
+      alert("sequence#4");
+    } else if (tally1 === tally2) {
+      alert("sequence#5");
+    } else if (tally2 === tally3) {
+      alert("sequence#6");
+    } else if (tally1 === tally3) {
+      alert("sequence#7");
+    }
   });
+
+
+
+
+});
